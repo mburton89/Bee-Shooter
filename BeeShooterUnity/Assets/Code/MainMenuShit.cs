989273e38efc2c6a3c006917c047d9caab3ff363
WUStudent <wustudent@webster.edu>
2023-04-26 16:48:51 -0500

    Added a menu

--- BeeShooterUnity/Assets/Code/MainMenuShit.cs
+++ BeeShooterUnity/Assets/Code/MainMenuShit.cs
@@ -7,17 +7,6 @@ public class MainMenuShit : MonoBehaviour
 {
     public string TheGame;
     public string Credits;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void StartGame()
     {
